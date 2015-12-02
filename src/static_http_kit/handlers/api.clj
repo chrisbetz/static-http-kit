615aa171884df701378466718a8093f839d63da4
Christian Betz <christian.betz@performance-media.de>
2015-12-02 10:09:25 +0100

    initializes project (equivalent to running "lein new http-kit static-http-kit --force")

---
+++ src/static_http_kit/handlers/api.clj
@@ -0,0 +1,6 @@
+(ns static-http-kit.handlers.api
+  )
+
+(defn get-time [req]
+  {:time (System/currentTimeMillis)
+   :req (merge req {:async-channel nil})})
---
+++ src/static_http_kit/tmpls.clj
@@ -0,0 +1,11 @@
+(ns static-http-kit.tmpls
+  (:use [static-http-kit.config :only [cfg]]
+        [me.shenfeng.mustache :only [gen-tmpls-from-resources]]))
+
+(defn add-info [data]
+  (assoc data
+    :dev? (= (cfg :profile) :dev)
+    :prod? (= (cfg :profile) :prod)))
+
+;;; generate clojure functions from src/templates folder
+(gen-tmpls-from-resources "templates" [".tpl"] add-info)
